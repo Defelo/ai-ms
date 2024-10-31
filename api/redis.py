ebd511c75a822f4b9c355b4037aebae23d98f9e5
hendrikse <jeanmarc.hendrikse@googlemail.com>
2024-10-31 16:02:08 +0100

    Fixed CI issues.

--- api/redis.py
+++ api/redis.py
@@ -10,9 +10,9 @@ logger = get_logger(__name__)
 
 # global redis connection
 logger.debug("initializing redis connection")
-redis: Redis = cast(Callable[..., Redis[Any]], from_url)(  # type: ignore
+redis: Redis = cast(Callable[..., Redis], from_url)(  # type: ignore
     settings.redis_url, encoding="utf-8", decode_responses=True
 )
-auth_redis: Redis = cast(Callable[..., Redis[Any]], from_url)(  # type: ignore
+auth_redis: Redis = cast(Callable[..., Redis], from_url)(  # type: ignore
     settings.auth_redis_url, encoding="utf-8", decode_responses=True
 )
